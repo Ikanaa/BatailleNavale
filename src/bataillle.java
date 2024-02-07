a41538fa884637e93b287126d02e1c823388bd9d
Louis Saffré <louis.saffre@info420.net>
2024-02-07 13:54:26 -0500

    Projet initial

---
+++ src/bataillle.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+
+/**
+ *
+ *      @author Louis Saffré
+ *      Sujet Jeu de Bataille Navale (Touché Coulé / BattleShip) !
+ *      @version 0.1
+ *      Description   :
+ *          Création d'un jeu de bataille navale tel que le jeux de société <a href="https://www.wikiwand.com/fr/Bataille_navale_(jeu)">Bataille Navale</a> !
+*/
+public class bataillle {
+    public static int [ ][ ] grilleOrdi = new int [10] [10];
+    public static int [ ][ ] grilleJeu = new int [10] [10];
+
+    public static boolean positionValide (int [ ] [ ] grille, int ligne, int colonne, int direction, int t)
+    {
+        return true;
+    }
+}
